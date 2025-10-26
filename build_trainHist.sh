af75d96c8a8bf9d8cfc3ea2c1334cda594dd94aa
hayato-n8810 <s276185@wakayama-u.ac.jp>
2025-10-26 15:12:19 +0900

    fix: ファイル名指定その2

--- build_trainHist.sh
+++ build_trainHist.sh
@@ -13,9 +13,6 @@ TRAIN_DATA_FILE=${DATASET_NAME}.train.raw.txt
 mkdir -p data
 mkdir -p data/${DATASET_NAME}
 
-TARGET_HISTOGRAM_FILE=data/${DATASET_NAME}/${DATASET_NAME}.histo.tgt.c2v
-ORIGIN_HISTOGRAM_FILE=data/${DATASET_NAME}/${DATASET_NAME}.histo.ori.c2v
-PATH_HISTOGRAM_FILE=data/${DATASET_NAME}/${DATASET_NAME}.histo.path.c2v
 # 特徴抽出
 echo "Extracting paths from training set..."
 ${PYTHON} JSExtractor/extract.py --dir ${TRAIN_DIR} --max_path_length 8 --max_path_width 2 | shuf > ${TRAIN_DATA_FILE}
@@ -27,7 +24,7 @@ mv -f ${TRAIN_DATA_FILE}.tmp ${TRAIN_DATA_FILE}
 # 語彙データ作成
 echo "Creating histograms from the training data"
 cat ${TRAIN_DATA_FILE} | cut -d' ' -f1 | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > ${TARGET_HISTO}
-cat ${TRAIN_DATA_FILE} | cut -d' ' -f2- | tr ' ' '\n' | cut -d',' -f1,3 | tr ',' '\n' | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > ${ORIGIN_HISTO}
+cat ${TRAIN_DATA_FILE} | cut -d' ' -f2- | tr ' ' '\n' | cut -d',' -f1,3 | tr ',' '\n' | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > ${WORD_HISTO}
 cat ${TRAIN_DATA_FILE} | cut -d' ' -f2- | tr ' ' '\n' | cut -d',' -f2 | awk '{n[$0]++} END {for (i in n) print i,n[i]}' > ${PATH_HISTO}
 
 echo "Finished creating histograms from the training data"
