8f5cb487bc99c459f36cda1b38d6223b7c735758
hayato-n8810 <s276185@wakayama-u.ac.jp>
2025-10-29 04:27:54 +0900

    類似度目視調査のためのファイルピックアップ

---
+++ ql2vec/view_result_222.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+# 上位10件 (インデックス 0-9)
+echo '/code2vec/target/id_222/Semantic-Org-Semantic-UI/Semantic-Org-Semantic-UI_4.js'
+cat /code2vec/target/id_222/Semantic-Org-Semantic-UI/Semantic-Org-Semantic-UI_4.js
+echo '/code2vec/target/id_222/pcottle-learnGitBranching/pcottle-learnGitBranching_0.js'
+cat /code2vec/target/id_222/pcottle-learnGitBranching/pcottle-learnGitBranching_0.js
+echo '/code2vec/target/id_222/AntSwordProject-antSword/AntSwordProject-antSword_53.js'
+cat /code2vec/target/id_222/AntSwordProject-antSword/AntSwordProject-antSword_53.js
+echo '/code2vec/target/id_222/nwjs-nw.js/nwjs-nw.js_6.js'
+cat /code2vec/target/id_222/nwjs-nw.js/nwjs-nw.js_6.js
+echo '/code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_63.js'
+cat /code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_63.js
+echo '/code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_88.js'
+cat /code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_88.js
+echo '/code2vec/target/id_222/anc95-ChatGPT-CodeReview/anc95-ChatGPT-CodeReview_29.js'
+cat /code2vec/target/id_222/anc95-ChatGPT-CodeReview/anc95-ChatGPT-CodeReview_29.js
+echo '/code2vec/target/id_222/anc95-ChatGPT-CodeReview/anc95-ChatGPT-CodeReview_3.js'
+cat /code2vec/target/id_222/anc95-ChatGPT-CodeReview/anc95-ChatGPT-CodeReview_3.js
+echo '/code2vec/target/id_222/WordPress-gutenberg/WordPress-gutenberg_2.js'
+cat /code2vec/target/id_222/WordPress-gutenberg/WordPress-gutenberg_2.js
+echo '/code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_25.js'
+cat /code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_25.js
+echo ''
+echo ''
+
+# 第3四分位数を中心とした10件 (位置: 1121) (インデックス 1116-1125)
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_203.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_203.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_229.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_229.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_24.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_24.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_255.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_255.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_281.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_281.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_307.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_307.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_332.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_332.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_50.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_50.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_75.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_75.js
+echo '/code2vec/target/id_222/react-syntax-highlighter-react-syntax-highlighter/react-syntax-highlighter-react-syntax-highlighter_118.js'
+cat /code2vec/target/id_222/react-syntax-highlighter-react-syntax-highlighter/react-syntax-highlighter-react-syntax-highlighter_118.js
+echo ''
+echo ''
+
+# 中央値を中心とした10件 (位置: 2243) (インデックス 2238-2247)
+echo '/code2vec/target/id_222/nextapps-de-flexsearch/nextapps-de-flexsearch_0.js'
+cat /code2vec/target/id_222/nextapps-de-flexsearch/nextapps-de-flexsearch_0.js
+echo '/code2vec/target/id_222/mediaelement-mediaelement/mediaelement-mediaelement_0.js'
+cat /code2vec/target/id_222/mediaelement-mediaelement/mediaelement-mediaelement_0.js
+echo '/code2vec/target/id_222/yui-yui3/yui-yui3_1065.js'
+cat /code2vec/target/id_222/yui-yui3/yui-yui3_1065.js
+echo '/code2vec/target/id_222/yui-yui3/yui-yui3_259.js'
+cat /code2vec/target/id_222/yui-yui3/yui-yui3_259.js
+echo '/code2vec/target/id_222/yui-yui3/yui-yui3_260.js'
+cat /code2vec/target/id_222/yui-yui3/yui-yui3_260.js
+echo '/code2vec/target/id_222/openwrt-luci/openwrt-luci_31.js'
+cat /code2vec/target/id_222/openwrt-luci/openwrt-luci_31.js
+echo '/code2vec/target/id_222/openwrt-luci/openwrt-luci_32.js'
+cat /code2vec/target/id_222/openwrt-luci/openwrt-luci_32.js
+echo '/code2vec/target/id_222/openwrt-luci/openwrt-luci_33.js'
+cat /code2vec/target/id_222/openwrt-luci/openwrt-luci_33.js
+echo '/code2vec/target/id_222/Tampermonkey-tampermonkey/Tampermonkey-tampermonkey_10.js'
+cat /code2vec/target/id_222/Tampermonkey-tampermonkey/Tampermonkey-tampermonkey_10.js
+echo '/code2vec/target/id_222/CesiumGS-cesium/CesiumGS-cesium_163.js'
+cat /code2vec/target/id_222/CesiumGS-cesium/CesiumGS-cesium_163.js
+echo ''
+echo ''
+
+# 第1四分位数を中心とした10件 (位置: 3364) (インデックス 3359-3368)
+echo '/code2vec/target/id_222/jsbin-jsbin/jsbin-jsbin_22.js'
+cat /code2vec/target/id_222/jsbin-jsbin/jsbin-jsbin_22.js
+echo '/code2vec/target/id_222/CesiumGS-cesium/CesiumGS-cesium_18.js'
+cat /code2vec/target/id_222/CesiumGS-cesium/CesiumGS-cesium_18.js
+echo '/code2vec/target/id_222/BrowserWorks-waterfox/BrowserWorks-waterfox_12.js'
+cat /code2vec/target/id_222/BrowserWorks-waterfox/BrowserWorks-waterfox_12.js
+echo '/code2vec/target/id_222/BrowserWorks-waterfox/BrowserWorks-waterfox_121.js'
+cat /code2vec/target/id_222/BrowserWorks-waterfox/BrowserWorks-waterfox_121.js
+echo '/code2vec/target/id_222/facebook-memlab/facebook-memlab_6.js'
+cat /code2vec/target/id_222/facebook-memlab/facebook-memlab_6.js
+echo '/code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_114.js'
+cat /code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_114.js
+echo '/code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_12.js'
+cat /code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_12.js
+echo '/code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_8.js'
+cat /code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_8.js
+echo '/code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_104.js'
+cat /code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_104.js
+echo '/code2vec/target/id_222/gatsbyjs-gatsby/gatsbyjs-gatsby_3.js'
+cat /code2vec/target/id_222/gatsbyjs-gatsby/gatsbyjs-gatsby_3.js
+echo ''
+echo ''
+
+# 下位10件 (インデックス 4476-4485)
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_322.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_322.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_40.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_40.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_65.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_65.js
+echo '/code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_91.js'
+cat /code2vec/target/id_222/krasimir-react-in-patterns/krasimir-react-in-patterns_91.js
+echo '/code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_122.js'
+cat /code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_122.js
+echo '/code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_18.js'
+cat /code2vec/target/id_222/mozilla-firefox-firefox/mozilla-firefox-firefox_18.js
+echo '/code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_14.js'
+cat /code2vec/target/id_222/ruanyf-react-demos/ruanyf-react-demos_14.js
+echo '/code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_110.js'
+cat /code2vec/target/id_222/tastejs-todomvc/tastejs-todomvc_110.js
+echo '/code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_1.js'
+cat /code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_1.js
+echo '/code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_17.js'
+cat /code2vec/target/id_222/bpampuch-pdfmake/bpampuch-pdfmake_17.js
+echo ''
+echo ''
+
